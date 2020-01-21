706ad6a978ea9266be9ae72b0f7f25a054076639
Michael Cramer <michael.cramer@bytecode.io>
2020-01-21 00:30:14 +0000

    Reduce pet growth cycles to 10m

--- MMOCoreORB/src/server/zone/objects/intangible/PetControlDeviceImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/intangible/PetControlDeviceImplementation.cpp
@@ -551,7 +551,7 @@ bool PetControlDeviceImplementation::growPet(CreatureObject* player, bool force,
 	Time currentTime;
 	uint32 timeDelta = currentTime.getTime() - lastGrowth.getTime();
 	//int stagesToGrow = timeDelta / 43200; // 12 hour
-	int stagesToGrow = timeDelta / 4800; // 10 minutes
+	int stagesToGrow = timeDelta / 600; // 10 minutes
 
 	if (adult)
 		stagesToGrow = 10;
