b6e982b86b35aa1b84ef0c301cf342f4677a156e
mikeface <obeedoob@benoobi.com>
2020-02-13 13:26:14 -0800

    Partial LS dmg redux at master defender

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -867,6 +867,7 @@ int CombatManager::getDefenderSecondaryDefenseModifier(CreatureObject* defender)
 
 float CombatManager::getDefenderToughnessModifier(CreatureObject* defender, int attackType, int damType, float damage) const {
 	ManagedReference<WeaponObject*> weapon = defender->getWeapon();
+	ManagedReference<PlayerObject*> ghost = defender->getPlayerObject();
 
 	const auto defenseToughMods = weapon->getDefenderToughnessModifiers();
 
@@ -881,10 +882,13 @@ float CombatManager::getDefenderToughnessModifier(CreatureObject* defender, int 
 	if ( attackType == SharedWeaponObjectTemplate::RANGEDATTACK && defender->isInCover()){
 		damage *= 1.f - ( 25.f / 100.f);
 	}
+	int numSkills = ghost->numSpecificSkills(defender, "force_discipline_defender_");
 
 	int jediToughness = defender->getSkillMod("jedi_toughness");
 	if (damType != SharedWeaponObjectTemplate::LIGHTSABER && jediToughness > 0)
 		damage *= 1.f - (jediToughness / 100.f);
+	if (damType == SharedWeaponObjectTemplate::LIGHTSABER && jediToughness > 0 && numSkills == 18)
+		damage *= 1.f - (jediToughness / 250.f);
 
 
 	if (damType == SharedWeaponObjectTemplate::LIGHTSABER && defender->isPlayerCreature() && defender->hasSkill("combat_bountyhunter_master")){
